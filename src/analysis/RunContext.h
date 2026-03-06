3024ee6db3b0f4538ce4fe126011c95de0fae531
liqing qin <qinliqing@physi.uni-heidelberg.de>
2026-03-06 18:45:05 +0100

    use board names (H1H2V1V2) in Pos2DMap and Pos1D for 2024

--- src/analysis/RunContext.h
+++ src/analysis/RunContext.h
@@ -12,31 +12,32 @@ struct RunContext
   std::string input_file;
   long max_frames;
 
-  const char* CAL_runname = "run19"; //used for generating calibration factors
-  const char* ALIGN_runname = "run17"; //used for align between boards
+  const char *CAL_runname = "run19";   // used for generating calibration factors
+  const char *ALIGN_runname = "run17"; // used for align between boards
 
   // Detector configuration
-  int nrBoards = 6; 
+  int nrBoards = 6;
   int nrChannels = 320;
-  int V_boardID[3] = {1,3,5}; //measuring vertical profile
-  int H_boardID[3] = {0,2,4}; //measuring horizontal profile
-  double readout_rate=10000; // in Hz; normal setting is 10 kHz
+  int BoardIP[6] = {17, 18, 19, 20, 21, 22};
+  const char *BoardName[6] = {"H0", "V0", "H1", "V1", "H2", "V2"};
+  int V_boardID[3] = {1, 3, 5}; // measuring vertical profile
+  int H_boardID[3] = {0, 2, 4}; // measuring horizontal profile
+
+  double readout_rate = 10000; // in Hz; normal setting is 10 kHz
 
   // Clustering setting for each board
-  int clustering_threshold[6] = {16, 16, 16, 16, 16, 16}; // up for 6 boards 2024 08 08 with common mode subtraction
   // int clustering_threshold[6] = {30, 15, 15, 30, 30, 30}; // up for 6 boards 2024 08 08
 
-  // int clustering_threshold[6] = {30, 30, 30, 15, 30, 30}; // up for 6 boards 2025 05 03
+  int clustering_threshold[6] = {16, 16, 16, 16, 16, 16}; 
+  bool common_mode[6] = {true, false, false, true, true, true};
 
   int clustering_size[6] = {4, 4, 4, 4, 4, 4}; // up for 6 boards
-
-  bool common_mode[6] = {true, false, false, true, true, true}; // common mode subtraction for 4 boards
 
   // FPGA Settings
   int FPGA_clustering_threshold[6] = {27, 13, 13, 29, 32, 29}; // up for 6 boards
   int FPGA_clustering_size[6] = {4, 4, 4, 4, 4, 4};            // up for 6 boards
-  bool FPGA_calibrated = true;                                // if it is FPGA calibrated
+  bool FPGA_calibrated = true;                                 // if it is FPGA calibrated
 
   // Analysis flags // for different analysis types
-  bool noise_run=false; // if it is a noise run, noise analyser will analyse all the frames
+  bool noise_run = false; // if it is a noise run, noise analyser will analyse all the frames
 };
--- src/analysis/modules/PosAlign.h
+++ src/analysis/modules/PosAlign.h
@@ -18,70 +18,106 @@ protected:
 
     void process(Fullframe &frame, long frame_index, FrameTags &tags) override
     {
-        // Position alignment logic goes here
+        (void)frame;
+        (void)frame_index;
+
         for (int i = 0; i < nrBoards; ++i)
         {
             if (tags.boardTags[i].Cluster_num < 1)
-            {
-                continue; // Skip processing if no cluster is found
-            }
-            tags.boardTags[i].Position = tags.boardTags[i].Position * slope_scaled[i] + intercept_scaled[i] - offset_origin;
+                continue;
+
+            tags.boardTags[i].Position =
+                tags.boardTags[i].Position * slope_scaled[i]
+                + intercept_scaled[i]
+                - offset_origin;
         }
     }
 
     void end_run(const RunContext &ctx) override
     {
+        (void)ctx;
     }
 
 private:
     double intercept_scaled[6] = {0, 0, 0, 0, 0, 0};
     double slope_scaled[6] = {1, 1, 1, 1, 1, 1};
+
     const double offset_origin = 128.4; // 128.4 mm set the origin of mm coordinate in the middle of the 320 channels
+
     void readIn_align(const RunContext &ctx);
 };
 
-void PosAlign::on_begin_run(const RunContext &ctx)
+inline void PosAlign::on_begin_run(const RunContext &ctx)
 {
-
     readIn_align(ctx);
 }
 
-void PosAlign::readIn_align(const RunContext &ctx)
+inline void PosAlign::readIn_align(const RunContext &ctx)
 {
     TFile *file = TFile::Open(Form("output/%s_Pos1D.root", ctx.ALIGN_runname), "READ");
-    if (!file)
+    if (!file || file->IsZombie())
     {
-        std::cout << Form("output/%s_Pos1D.root does not exist ", ctx.ALIGN_runname) << std::endl;
+        std::cout << Form("output/%s_Pos1D.root does not exist", ctx.ALIGN_runname) << std::endl;
+        return;
     }
 
-    TF1 *H_Fit[3]{};
-    TF1 *V_Fit[3]{};
-    for (int i = 0; i < nrBoards / 2; i++)
+    // reset defaults
+    for (int i = 0; i < 6; ++i)
     {
-        H_Fit[i] = (TF1 *)file->Get(Form("Pos1D/H%d_Fit", i));
-        V_Fit[i] = (TF1 *)file->Get(Form("Pos1D/V%d_Fit", i));
+        intercept_scaled[i] = 0.0;
+        slope_scaled[i] = 1.0;
+    }
 
-        if (!H_Fit[i])
+    // Horizontal boards
+    for (int i = 0; i < nrBoards / 2; ++i)
+    {
+        const int boardID = ctx.H_boardID[i];
+        const char *boardName = ctx.BoardName[boardID];
+
+        TF1 *fit = (TF1 *)file->Get(Form("Pos1D/%s_Fit", boardName));
+        if (!fit)
         {
-            std::cout << " can not find the TF1 fit " << std::endl;
-            return;
+            std::cout << "Cannot find TF1 fit for " << boardName << std::endl;
+            continue;
         }
 
-        if (i != 1) // H1 is the reference
+        // reference board keeps default (0,1)
+        if (i != 1)
         {
-            intercept_scaled[ctx.H_boardID[i]] = H_Fit[i]->GetParameter(0);
-            slope_scaled[ctx.H_boardID[i]] = H_Fit[i]->GetParameter(1);
-        }
-        if (i != 1) // H1 is the reference
-        {
-            intercept_scaled[ctx.V_boardID[i]] = V_Fit[i]->GetParameter(0);
-            slope_scaled[ctx.V_boardID[i]] = V_Fit[i]->GetParameter(1);
+            intercept_scaled[boardID] = fit->GetParameter(0);
+            slope_scaled[boardID] = fit->GetParameter(1);
         }
     }
 
-    for (int i = 0; i < 6; i++)
+    // Vertical boards
+    for (int i = 0; i < nrBoards / 2; ++i)
     {
-        std::cout << "alignment factor: " << intercept_scaled[i] << " " << slope_scaled[i] << std::endl;
+        const int boardID = ctx.V_boardID[i];
+        const char *boardName = ctx.BoardName[boardID];
+
+        TF1 *fit = (TF1 *)file->Get(Form("Pos1D/%s_Fit", boardName));
+        if (!fit)
+        {
+            std::cout << "Cannot find TF1 fit for " << boardName << std::endl;
+            continue;
+        }
+
+        // reference board keeps default (0,1)
+        if (i != 1)
+        {
+            intercept_scaled[boardID] = fit->GetParameter(0);
+            slope_scaled[boardID] = fit->GetParameter(1);
+        }
     }
+
+    for (int i = 0; i < nrBoards; ++i)
+    {
+        std::cout << ctx.BoardName[i]
+                  << " alignment factor: "
+                  << intercept_scaled[i] << " "
+                  << slope_scaled[i] << std::endl;
+    }
+
     file->Close();
+    delete file;
 }
